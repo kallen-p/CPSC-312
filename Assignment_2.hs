843bcc241fbfc81b4261406693b0722d2c41417d
Kallen Paterson <kallenpaterson@gmail.com>
2023-01-23 20:07:23 -0800

    Finished question 1a and parts i,ii,iii for 1b

--- Assignment_2.hs
+++ Assignment_2.hs
@@ -1,1 +1,25 @@
 -- Assignment 2
+
+
+{-1a.
+   i.   tails :: [a] -> [[a]]
+   ii.  tails "happy" = [["happy"], ["appy"], ["ppy"] ["py"], ["y"]]
+   iii. tails "happy" :: [[Char]]
+   iv.   No it is not possible to define tails as a foldr or foldl without using recursion. This is because tails relies appending the whole list with each 
+         subset of the list until it reaches the empty set. i.e each iteration has to go through tails.
+  
+  b.
+   i.   doif :: (Bool -> a) -> (a -> a) -> [a] -> [a]
+   ii.  doif even (‘div‘ 2) [11,22,33,44,55,66] = [11,11,33,22,55,33] :: [Int] -}
+-- iii. 
+
+doif f g [] = []
+doif f g (h:t)
+ | f h = g h : doif f g t
+ | otherwise = h : doif f g t
+ 
+toUpper :: Char -> Char
+toUpper x = toEnum( fromEnum x - fromEnum 'a' + fromEnum 'A')
+ 
+capVowel [] = []
+capVowel (h:t) = doif (\ x -> x `elem` ['a','e','i','o','u']) toUpper (h:t)
